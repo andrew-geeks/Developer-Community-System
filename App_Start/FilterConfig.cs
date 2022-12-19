826d1c13b640035b37491e54fbfe38438dff21f1
Andrew George Issac <andrewgeorgeissac@gmail.com>
2022-12-19 20:06:40 +0530

    Add files via upload

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace DCS_new
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
